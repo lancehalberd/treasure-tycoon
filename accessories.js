d06c79c30ebb61fcdd3ae6248e7e1b7563e090f2
chrisb <chrisb@mode.com>
2016-11-30 21:20:35 -0600

    Make enemies a little weaker.

--- accessories.js
+++ accessories.js
@@ -1,12 +1,13 @@
 
 //Back/Neck/Waist
-addItem(1, {'slot': 'back', 'type': 'quiver', 'name': 'Quiver', 'bonuses': {'+bow:minPhysicalDamage': 2, '+bow:maxPhysicalDamage': 4}, icon: 'quiver'});
-addItem(2, {'slot': 'back', 'type': 'scabbard', 'name': 'Scabbard', 'bonuses': {'+dagger:minPhysicalDamage': 1, '+dagger:maxPhysicalDamage': 2, '+sword:minPhysicalDamage': 2, '+sword:maxPhysicalDamage': 3, '+greatsword:minPhysicalDamage': 3, '+greatsword:maxPhysicalDamage': 4}, icon: 'scabbard'});
-addItem(3, {'slot': 'back', 'type': 'baldric', 'name': 'Baldric', 'bonuses': {'+axe:minPhysicalDamage': 2, '+axe:maxPhysicalDamage': 3, '+polearm:minPhysicalDamage': 3, '+polearm:maxPhysicalDamage': 5}, icon: 'scabbard'});
+addItem(1, {'slot': 'back', 'type': 'cloak', 'name': 'Plain Cloak', 'bonuses': {'+evasion': 1, '+armor': 1, '+maxHealth': 6}, icon: 'cloak'});
+addItem(2, {'slot': 'back', 'type': 'quiver', 'name': 'Quiver', 'bonuses': {'+bow:minPhysicalDamage': 2, '+bow:maxPhysicalDamage': 4}, icon: 'quiver'});
+addItem(3, {'slot': 'back', 'type': 'scabbard', 'name': 'Scabbard', 'bonuses': {'+dagger:minPhysicalDamage': 1, '+dagger:maxPhysicalDamage': 2, '+sword:minPhysicalDamage': 2, '+sword:maxPhysicalDamage': 3, '+greatsword:minPhysicalDamage': 3, '+greatsword:maxPhysicalDamage': 4}, icon: 'scabbard'});
+addItem(4, {'slot': 'back', 'type': 'baldric', 'name': 'Baldric', 'bonuses': {'+axe:minPhysicalDamage': 2, '+axe:maxPhysicalDamage': 3, '+polearm:minPhysicalDamage': 3, '+polearm:maxPhysicalDamage': 5}, icon: 'scabbard'});
+addItem(5, {'slot': 'back', 'type': 'amulet', 'name': 'Runed Amulet', 'bonuses': {'+wand:minMagicDamage': 1, '+wand:maxMagicDamage': 2, '+staff:minMagicDamage': 2, '+staff:maxMagicDamage': 3}, icon: 'amulet'});
 addItem(15, {'slot': 'back', 'type': 'cloak', 'name': 'Travelers Cloak', 'bonuses': {'+evasion': 10}, icon: 'cloak'});
 addItem(16, {'slot': 'back', 'type': 'cloak', 'name': 'Fur Cloak', 'bonuses': {'+maxHealth': 60}, icon: 'cloak'});
 addItem(17, {'slot': 'back', 'type': 'cloak', 'name': 'Plated Cloak', 'bonuses': {'+armor': 10}, icon: 'cloak'});
-addItem(20, {'slot': 'back', 'type': 'amulet', 'name': 'Runed Amulet', 'bonuses': {'+wand:minMagicDamage': 1, '+wand:maxMagicDamage': 2, '+staff:minMagicDamage': 2, '+staff:maxMagicDamage': 3}, icon: 'amulet'});
 
 
 //Rings
@@ -20,4 +21,7 @@ addItem(12, {'slot': 'ring', 'type': 'ring', 'name': 'Topaz Ring', 'bonuses': {'
 addItem(13, {'slot': 'ring', 'type': 'ring', 'name': 'Aquamarine Ring', 'bonuses': {'+intelligence': 3, '+dexterity': 3}, icon: 'ring'});
 addItem(14, {'slot': 'ring', 'type': 'ring', 'name': 'Amethyst Ring', 'bonuses': {'+strength': 3, '+intelligence': 3}, icon: 'ring'});
 addItem(19, {'slot': 'ring', 'type': 'ring', 'name': 'Diamond Ring', 'bonuses': {'+strength': 2, '+dexterity': 2, '+intelligence': 2}, icon: 'ring'});
-addItem(20, {'slot': 'ring', 'type': 'ring', 'name': 'Orchalcum Band', 'bonuses': {'+healthRegen': 4}, icon: 'band'});
+addItem(20, {'slot': 'ring', 'type': 'ring', 'name': 'Steel Band', 'bonuses': {'%physicalDamage': .2}, icon: 'band'});
+addItem(21, {'slot': 'ring', 'type': 'ring', 'name': 'Jewel Band', 'bonuses': {'%magicDamage': .2}, icon: 'band'});
+addItem(22, {'slot': 'ring', 'type': 'ring', 'name': 'Stone Band', 'bonuses': {'%melee:damage': .2}, icon: 'band'});
+addItem(23, {'slot': 'ring', 'type': 'ring', 'name': 'Wood Band', 'bonuses': {'%ranged:damage': .2}, icon: 'band'});
